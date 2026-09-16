c20ccf5ac9bb52f4283bd4bf1f918c24c21f4102
Johl Brown <johlbrown@gmail.com>
2026-09-16 18:53:10 +1000

    YM RED: demand source covariance to vacuum form gap weld

---
+++ Welds/YMSourceClusteringGapRegression.lean
@@ -0,0 +1,12 @@
+import Welds.YMSourceClusteringGap
+
+namespace Welds.YMSourceClusteringGapRegression
+
+open scoped InnerProductSpace
+open RequestProject.YangMills.VacuumSectorSpectralGap
+
+variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℂ E]
+
+#check Welds.YMSourceClusteringGap.vacuumFormGapOfSourceCovariance
+
+end Welds.YMSourceClusteringGapRegression

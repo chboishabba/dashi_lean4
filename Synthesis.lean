ab6373838cbabb0def8a888c4e3fc37f4e2d38d2
Johl Brown <johlbrown@gmail.com>
2026-09-16 12:25:17 +1000

    Wire RH quartic far-shell donor into synthesis root

--- Synthesis.lean
+++ Synthesis.lean
@@ -16,6 +16,8 @@ import Synthesis.ExponentFibre
 import Synthesis.MonsterOrder
 import Synthesis.MonsterCharacterDetermination
 import Synthesis.MonsterCharacterMultiplicityRegression
+import Synthesis.RiemannFarShellQuarticCutoff
+import Synthesis.RiemannFarShellQuarticCutoffRegression
 import Synthesis.Hyperfabric
 import Synthesis.CodecBridge
 import Synthesis.EndToEnd
